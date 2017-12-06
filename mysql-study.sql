fdd13c19d5fcf315fcff171f16dae0106f38dfda
xkong <18551061885@163.com>
2017-12-06 20:35:38 +0800

    init repo & add tow sql files

---
+++ mysql-study.sql
@@ -0,0 +1,35 @@
+# 单行注释
+-- 单行注释
+/**/ 多行注释
+
+# 行级锁只在存储引擎上实现
+# 表锁是最基本的所策略，并且是开销最小的策略
+
+USE playboxjre;
+DESC xiaochi;
+
+-- ***事务***
+# ACID 
+/*
+* A atomicity 	原子性
+* C consistency 一致性
+* I isolation 	隔离性
+* D durability 	持久性
+*/
+START TRANSACTION;
+SELECT * FROM xiaochi;
+INSERT INTO xiaochi (username,`password`,age) VALUE ("kong","123",12);
+COMMIT;		#不写，也可以insert语句也可以插入成功
+# rollback; 	回滚（也就是insert 语句没有生效）
+
+-- ***隔离级别***
+
+# 隔离级别		脏读可能性	不可重复读可能性	幻读可能性	加锁读
+# READ UNCOMMITED 	YES		YES			YES		NO
+# READ COMMITED		NO		YES			YES		NO	
+# REPEATABLE READ	NO		NO			YES		NO
+# SERIALIZABLE		NO		NO			NO		YES
+
+
+
+SELECT * FROM xiaochi;
---
+++ mysql-synax.sql
@@ -0,0 +1,14 @@
+# 查看用户和时间信息
+SELECT CURRENT_USER;		# root@localhost
+SELECT CURRENT_DATE;		# 2017-12-05
+SELECT CURRENT_TIME;	 	# 20:21:53
+SELECT CURRENT_TIMESTAMP; 	# 2017-12-05 20:21:15
+SELECT NOW(); 			# 2017-12-05 20:21:15
+
+# 使用数据库
+USE `playboxjre`;			
+
+# 查看表结构
+DESCRIBE `xiaochi`;
+DESC xiaochi;
+
